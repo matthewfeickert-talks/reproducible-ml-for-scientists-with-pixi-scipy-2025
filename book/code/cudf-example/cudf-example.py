91baf1e582068b1fe613cebae0a07049baeba0ee
jakirkham <jakirkham@gmail.com>
2025-07-07 12:52:07 -0700

    Compute number of pages in English

--- book/code/cudf-example/cudf-example.py
+++ book/code/cudf-example/cudf-example.py
@@ -13,3 +13,10 @@ print(f"Pandas DataFrame:\n {df_cpu.head()}")
 # also exist with CuDF
 df_gpu = cudf.read_csv(data_url, sep=" ")
 print(f"\nCuDF DataFrame:\n {df_gpu.head()}")
+
+# Label columns & drop unused column
+df_gpu.columns = ["project", "page", "requests", "x"]
+df_gpu = df_gpu.drop("x", axis=1)
+
+# Count number of English pages
+print(f"\n# of English:\n {df_gpu[df_gpu.project == 'en'].count()}")
